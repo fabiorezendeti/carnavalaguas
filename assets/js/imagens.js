19ff93507d7bfd4d0562eee0c09505b4ef6ed16f
fabiorezendeti <fabiorezendeti@gmail.com>
2020-03-02 10:01:18 -0300

    teste

---
+++ assets/js/imagens.js
@@ -0,0 +1,21 @@
+﻿   // Masonry Grid
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 180,
+        fitWidth: true, // When enabled, you can center the container with CSS.
+        gutter: 10
+    });
+
+    // Loading Animation
+    $('.grid-item').each(function (i) {
+        setTimeout(function () {
+            $('.grid-item').eq(i).addClass('is-visible');
+        }, 200 * i);
+    });
+
+    // Fancybox
+    $('.fancybox').fancybox({
+        helpers: {
+            overlay: { locked: false }
+        }
+    });
---
+++ vr-ram-gh-pages/README.md
@@ -0,0 +1,1 @@
+A-Frame based VR experience demo.
